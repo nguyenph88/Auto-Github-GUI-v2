737d5f66caaf760c3cf3d652de17656896b9c252
Peter Nguyen <nguyenph88@gmail.com>
2015-01-14 12:32:34 -0800

    final version without threading

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class AutoGithubTool(MainFormAG.MainFrame):
 	def __init__(self,parent):
 		#initialize parent class
 		MainFormAG.MainFrame.__init__(self,parent)
-		self.driver = webdriver.Firefox()
 
 	def buttonStartEvent(self, event):
 		self.isFollow = self.boxFollow.GetValue()
@@ -16,25 +15,60 @@ class AutoGithubTool(MainFormAG.MainFrame):
 		self.isFork = self.boxFork.GetValue()
 		self.quantity = self.fieldQuantity.GetValue()
 		self.profile = self.fieldProfile.GetValue()
-		self.repos = self.fieldRepos.GetValue()
+		self.repos = [x.strip() for x in self.fieldRepos.GetValue().split(',')]
 		self.accountList = self.getAccountList()
+		self.profile_user = self.profile.split("/")[3]
 
-		self.start()
+		# ignore the sanity check that quanlity > num account
+		for i in range(0, int(self.quantity)):
+			self.start(i)
+			#thread.start_new_thread( self.start, ("i",) )
+
+		self.statusBar.SetStatusText("Done!")
 
 	def getAccountList(self):
 		return [line.strip() for line in open("users.txt")]
 
-	def start(self):
-		# ignore the sanity check that quanlity > num account
-		for i in range(0, int(self.quantity)):
-			parts = self.accountList[i].split("|")
-			username = parts[0]
-			password = parts[1]
-			if self.isFollow:
-				self.doFollow(username, password)
-
-	def doFollow(self, username, password):
+	def start(self, i):
+		driver = webdriver.Firefox()
+		parts = self.accountList[i].split("|")
+		username = parts[0]
+		password = parts[1]
+		self.doLogin(driver, username, password)
+		if self.isFollow:
+			self.doFollow(driver)
+		if self.isStar:
+			self.doStar()
+		if self.isFork:
+			self.doFork()
 		
+		driver.close()
+
+	def doLogin(self, driver, username, password):
+		self.statusBar.SetStatusText("Login:" + username + " / " + password)
+		driver.get("https://github.com/login")
+		element = driver.find_element_by_id("login_field")
+		element.send_keys(username)
+		element = driver.find_element_by_id("password")
+		element.send_keys(password)
+		element.submit()
+
+	def doFollow(self, driver):
+		self.statusBar.SetStatusText("Following " + self.profile)
+		driver.get(self.profile)
+		element = driver.find_element_by_class_name("js-toggler-form")
+		element.submit()
+
+	def doStart(self):
+		for repo in self.repos:
+			self.statusBar.SetStatusText("Staring: " + repo)
+			driver.get(self.profile + repo)
+			xpath = "//form[@action='/" + self.profile_user + "/" + repo + "/star']"
+			element = driver.find_element_by_xpath(xpath)
+			element.submit()
+
+	def doFork():
+		pass
 
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+test = "http://www.github.com/npploveyou/"
+
+parts = test.split("/")
+
+asw = test.split("/")[3]
+print asw
